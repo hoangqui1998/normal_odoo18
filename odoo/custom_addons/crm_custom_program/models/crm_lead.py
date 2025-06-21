9a1a6d4e3ee75d3e3b7d6f4116cf928cb3c4cef1
QuiPhan <hoangqui12a7@gmail.com>
2025-06-21 20:54:45 +0700

    enhance 3 field required

--- odoo/custom_addons/crm_custom_program/models/crm_lead.py
+++ odoo/custom_addons/crm_custom_program/models/crm_lead.py
@@ -3,14 +3,33 @@ from odoo import models, fields, api
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    x_rate_type = fields.Selection([
-        ('fixed', 'Fixed'),
-        ('adjustable', 'Adjustable')
-    ], string='Rate Type')
+    x_loan_type = fields.Selection([
+        ('conventional', 'Conventional'),
+        ('fha', 'FHA'), 
+        ('USDA_rural_housing', 'USDA Rural Housing'),
+        ('va', 'VA'),
+        ('heloc', 'HELOC'),
+        ('heloan', 'HELOAN'),
+        ('others', 'Others')
+    ], string='Loan Type')
+
+    x_rate_type = fields.Many2one(
+        'crm.program.config',
+        string='Rate Type',
+    )
 
     x_program = fields.Many2one(
         'crm.program.config',
         string='Program',
     )
 
+    @api.onchange('x_loan_type')
+    def _onchange_x_loan_type(self):
+        self.x_rate_type = False
+
+    @api.onchange('x_rate_type')
+    def _onchange_x_rate_type(self):
+        self.x_program = False
+
+    
 
--- odoo/custom_addons/crm_custom_program/models/crm_program_config.py
+++ odoo/custom_addons/crm_custom_program/models/crm_program_config.py
@@ -6,7 +6,23 @@ class CrmProgramConfig(models.Model):
     _description = 'CRM Program Configuration'
 
     name = fields.Char(string="Name", required=True)
-    x_rate_type = fields.Selection([
-        ('fixed', 'Fixed'),
-        ('adjustable', 'Adjustable')
-    ], string='Rate Type', required=True)
+
+    x_loan_type = fields.Selection([
+        ('conventional', 'Conventional'),
+        ('fha', 'FHA'), 
+        ('USDA_rural_housing', 'USDA Rural Housing'),
+        ('va', 'VA'),
+        ('heloc', 'HELOC'),
+        ('heloan', 'HELOAN'),
+        ('others', 'Others')
+    ], string='Loan Type', required=True)
+
+    x_rate_type = fields.Many2one(
+        'crm.program.config',
+        string='Rate Type',
+    )
+
+    x_program = fields.Many2one(
+        'crm.program.config',
+        string='Program',
+    )
